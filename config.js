c4801ec7977eb25e8b6285a0b36c307517ea3865
DrasisAutomation <drasisdata@gmail.com>
2025-11-10 16:59:26 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCmFOzpOnj56qPsJmOeH_fGbHfNaNMrKKI",
+    authDomain: "lumi-configurator.firebaseapp.com",
+    projectId: "lumi-configurator",
+    storageBucket: "lumi-configurator.firebasestorage.app",
+    messagingSenderId: "302539320691",
+    appId: "1:302539320691:web:19917286b3519f66d4a906",
+    measurementId: "G-YWNWB9RLKJ"
+};
